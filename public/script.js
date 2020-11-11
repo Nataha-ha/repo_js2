c7db2117d83777702fe99b0df718b93e790cd72e
Nataha-ha <nataly.matveeva95@gmail.com>
2020-11-11 22:52:03 +0400

    task lesson1

---
+++ public/script.js
@@ -0,0 +1,32 @@
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    }
+];
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>Цена: ${price}</p></div>`;
+
+};
+
+const renderGoodsList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+    document.querySelector('.goods-list').style.display = 'flex';
+    document.querySelector('.goods-list').style.flexWrap = 'wrap';
+
+};
+
+renderGoodsList(goods);
