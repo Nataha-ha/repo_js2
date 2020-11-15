ec2b09829792ab51135c332003ee4ea6cbed9984
Nataha-ha <nataly.matveeva95@gmail.com>
2020-11-15 21:02:18 +0400

    server.js modified

--- server.js
+++ server.js
@@ -7,17 +7,29 @@ const server = http.createServer((req, res) => {
     //     fs.readFileSync('./public/script.js') :
     //     fs.readFileSync('./public/index.html');
     // res.end(body);
-    let body = '';
-    switch (req.url) {
-        case '/':
-            body = fs.readFileSync('./public/index.html');
-            break;
-        case '/script.js':
-            body = fs.readFileSync('./public/script.js');
-            break;
-        case '/style.css':
-            body = fs.readFileSync('./public/style.css');
-            break;
+
+    // let body = '';
+    // switch (req.url) {
+    //     case '/':
+    //         body = fs.readFileSync('./public/index.html');
+    //         break;
+    //     case '/script.js':
+    //         body = fs.readFileSync('./public/script.js');
+    //         break;
+    //     case '/style.css':
+    //         body = fs.readFileSync('./public/style.css');
+    //         break;
+    // }
+    // res.end(body);
+
+    const publicPath = './public';
+
+    let body = null;
+    try {
+        body = fs.readFileSync(`${publicPath}${req.url}`);
+    } catch (e) {
+        console.log(e);
+        body = fs.readFileSync(`${publicPath}/index.html`);
     }
     res.end(body);
 });
