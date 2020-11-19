a04e93f4554211c5d221ed87c2a664d385dfbe45
Nataha-ha <nataly.matveeva95@gmail.com>
2020-11-19 20:15:31 +0400

    buttons and styles added

--- public/shop.js
+++ public/shop.js
@@ -38,8 +38,9 @@ class List {
 }
 
 class GoodItem {
-    name = ''
-    price = 0
+    name = '';
+    price = 0;
+    count = 1;
 
     constructor({
         name,
@@ -53,28 +54,60 @@ class GoodItem {
         const placeToRender = document.querySelector('.goods-list');
         if (placeToRender) {
             const block = document.createElement('div');
-            block.innerHTML = `Товар: ${this.name} = ${this.price}`;
+            block.classList.add('block-items');
+            block.innerHTML = `<img src="" alt="product_photo"><div>${this.name}</div><div>${this.price}${' &#8381'}</div>`;
             placeToRender.appendChild(block);
+            block.appendChild(this.btn());
         }
     }
+
+    btn() {
+        const placeToRenderBtn = document.querySelector('.block-items');
+        if (placeToRenderBtn) {
+            const btn = document.createElement('button');
+            btn.classList.add('button-buy');
+            btn.innerHTML = 'Добавить в корзину';
+            btn.addEventListener('click', () => {
+                const CartInstanse = new Cart();
+
+            });
+            return btn;
+        }
+
+    }
+
 }
 
+class Cart extends List {
+    constructor() {
+        super();
+        this.cartTemplate();
+
+    }
+
+
+    cartTemplate() {
+        const cart = document.querySelector('.cart');
+        const blockBtn = document.createElement('button');
+        blockBtn.classList.add('cart-btn');
+        blockBtn.innerText = 'Купить';
+        const blockCart = document.createElement('div');
+        blockCart.classList.add('block-cart');
+        blockCart.innerText = 'В корзине пусто';
+        cart.appendChild(blockBtn);
+        cart.appendChild(blockCart);
+        this.btn();
+
+
+
+
+    }
+
+    cartRender() {
+        // this.cartTemplate();
+
+    }
+}
 
 const ListInstance = new List();
-
-class Cart {
-    constructor() {
-        items = [];
-        this.render();
-    }
-
-    addCartItem(cartItem) {
-        this.items.push(cartItem);
-
-    }
-
-    render() {
-        const placeToRenderCart = document.querySelector('.cart');
-
-    }
-}
+// const CartInstanse = new Cart();
